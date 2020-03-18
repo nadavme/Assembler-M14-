6a0b3254bc35615875d0f57b99a8c34c88cf9776
yairetkes <yair.etkes@gmail.com>
2020-03-18 20:02:37 +0200

    adding the best freaking comments

--- firstRunOver.h
+++ firstRunOver.h
@@ -10,41 +10,41 @@
 
 typedef struct first_word /*we've choosed to define the machine language word with bit fields, lets see how it goes... Nadav The king, Matan the princess ;-) */
 {
-    unsigned short external : 1;
-    unsigned short relocatable : 1;
-    unsigned short absolute : 1;
+    unsigned short external : 1;/*E*/
+    unsigned short relocatable : 1; /*R*/
+    unsigned short absolute : 1;/*A*/
 
-    unsigned short destination_0 : 1;
-    unsigned short destination_1 : 1;
-    unsigned short destination_2 : 1;
-    unsigned short destination_3 : 1;
+    unsigned short destination_0 : 1; /*this bit is turned on iff method 0 is used for destination operand */
+    unsigned short destination_1 : 1; /*this bit is turned on iff method 1 is used for destination operand */
+    unsigned short destination_2 : 1; /*this bit is turned on iff method 2 is used for destination operand */
+    unsigned short destination_3 : 1; /*this bit is turned on iff method 3 is used for destination operand */
 
-    unsigned short source_0 : 1;
-    unsigned short source_1 : 1;
-    unsigned short source_2 : 1;
-    unsigned short source_3 : 1;
+    unsigned short source_0 : 1; /*this bit is turned on iff method 0 is used for source operand */
+    unsigned short source_1 : 1; /*this bit is turned on iff method 1 is used for source operand */
+    unsigned short source_2 : 1; /*this bit is turned on iff method 2 is used for source operand */
+    unsigned short source_3 : 1; /*this bit is turned on iff method 3 is used for source operand */
 
-    unsigned int opCode : 4;
+    unsigned int opCode : 4; /*this bits will contain the operation code*/
 }first_word;
 
-typedef struct second_word /*word for 1st and 2nd methods (mio'n methods)*/
+typedef struct second_word /*word for methods 0,1 (mio'n methods)*/
 {
 
 
-    unsigned short op_is_external : 1;
-    unsigned short op_is_relocatable : 1;
-    unsigned short op_is_absolute : 1;
+    unsigned short op_is_external : 1; /*E*/
+    unsigned short op_is_relocatable : 1; /*R*/
+    unsigned short op_is_absolute : 1; /*A*/
 
     unsigned short num_or_addres : 12;/*in this 12 bits we'll store the actual value or the memory addres.*/
 }second_word;
 
-typedef struct third_word /*word for 3rd and 4th methods (mio'n methods)*/
+typedef struct third_word /*word for methods 2,3 (mio'n methods)*/
 {
 
 
-    unsigned short op_is_external : 1;
-    unsigned short op_is_relocatable : 1;
-    unsigned short op_is_absolute : 1;
+    unsigned short op_is_external : 1; /*E*/
+    unsigned short op_is_relocatable : 1; /*R*/
+    unsigned short op_is_absolute : 1; /*A*/
 
     unsigned short destination_register : 3;/*this bits will contain the number of register which has the 
                                                                             memory addres of the value of the destination operand. */
