3befe4344ea43dd57bcbfae84717da1c08b9cd95
yairetkes <yair.etkes@gmail.com>
2020-03-21 23:54:09 +0200

    function bin_to_octal is finished!!

---
+++ testers/tester_for_bin_to_octal.c
@@ -0,0 +1,33 @@
+//
+// this a tester for bin to octal
+//
+
+#include "aidTools.h" 
+#include <stdio.h>
+#include <math.h>
+
+
+int bin_to_octal(int binaryNum)/*this function converts a number from binary base to octal base, I used a method that we have learned in class.*/
+{
+    int i = 0,j = 0;
+    int sum = 0;
+    int midSum = 0;
+
+    while(binaryNum!=0)
+    {
+		midSum = 0;
+        for (j = 0; j < 3; j++)/*the function basically looks at the 3 most right bits, and check for each bit from right to left - if it's on we add 1 
+		multyplied by 2 to the power of the place of the bit (0/1/2) to midSum.*/
+        {
+            if( binaryNum&1 == 1)
+             {
+                midSum =midSum + (int)pow(2,j);
+             }
+        binaryNum = binaryNum>>1;
+        }
+        sum = sum+ (int)midSum*pow(10,i);/*we add midSum that fit the right digit*/
+        i++;
+
+    }
+    return sum;
+}
