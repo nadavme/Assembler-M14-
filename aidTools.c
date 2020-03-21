a54002821ed4fe3b9e0042d3acfb7b7d2462fb77
yairetkes <yair.etkes@gmail.com>
2020-03-21 23:54:59 +0200

    function bin_to_octal is finished!!!!!

--- aidTools.c
+++ aidTools.c
@@ -2,28 +2,87 @@
 // Created by nadav on 04/02/2020.
 //
 
-#include "aidTools.h"
+#include "aidTools.h" 
+#include <stdio.h>
+#include <math.h>
 
 
 int bin_to_octal(int binaryNum)
 {
-long int octalnum = 0, j = 1, remainder;
- 
-    while (binarynum != 0)
+    int i = 0,j = 0;
+    int sum = 0;
+    int midSum = 0;
+
+    while(binaryNum!=0)
     {
-        remainder = binarynum % 10;
-        octalnum = octalnum + remainder * j;
-        j = j * 2;
-        binarynum = binarynum / 10;
+		midSum = 0;
+        for (j = 0; j < 3; j++)
+        {
+            if( binaryNum&1 == 1)
+             {
+                midSum =midSum + (int)pow(2,j);
+             }
+        binaryNum = binaryNum>>1;/*possible failure*/
+        }
+        sum = sum+ (int)midSum*pow(10,i);
+        i++;
+
     }
-    printf("Equivalent octal value: %lo", octalnum);
-    return 0;
+    return sum;
 }
 
-int main()
+void reverse (char s[])
+ /* reverse character string, a function from the book, ex. 1-19*/
 {
-    long int num = 10;
-    printf("the number in binary is - %d and in octal - %d\n", (int)num, (int)bin_to_octal(num));
+	 int  string_length;
+	 int i;
+	 char tmp; /*  step  1:  calculate  length  of s */
+	 string_length = 0;
+	 while  (s[string_length]  !=  '\0')
+		 string_length = string_length + 1 ;
+		 /* step 2: reverse string */ 
+	for(i=0;i<string_length/2; i++) 
+	{
+		tmp=s[i];
+		s[i]=s[string_length-i-1];
+		s[string_length-i-1]=tmp ;
+	}
+}
+
+int translate_dec_hex(char dec[]) /*requested function*/
+{
+	char binNum[100]= {'0','\0'}; /*creating a char array (a string) which will express the binary 	       number.  the first cell in the array contains the char 0 representation, and all other cells in the 			array 	contain of the integer 0.*/
+
+	unsigned int decInt = strtoul(dec,NULL,10);/*converting the decimal number (string) to int*/
+	int i = 0;
+	while (decInt != 0)
+	{
+		binNum[i] ='0' + decInt%2; /*collecting the remainders from devision by 2 at the array*/
+		decInt = decInt/2; /*deviding by 2 (basically cutting out the fractions)*/
+		i++;		
+	}
+	
+	reverse(binNum); /*we have to reverse the array to get the binary number, as described at class*/
+		printf("%s\n",binNum);/*printing the binary number*/
+	
+	
+	return 0;
+}
+
+
+
+
+int main()/*to compile it I need to run gcc aidTools.c -lm -o a.out
+						and than ./a.out*/
+{
+    int num = 126;
+
+    char str[12];
+    sprintf(str, "%d", num);
+
+    printf("the number %d in binary is -\n" ,num);
+    translate_dec_hex(str);
+    printf("\n and in octal - %d\n", (int)bin_to_octal(num));
     return 0;
 }
 
