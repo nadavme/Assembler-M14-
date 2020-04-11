46b6a5c7ba88ea7d46536bd53d3f14897118889c
yairetkes <yair.etkes@gmail.com>
2020-04-11 20:45:31 +0300

    symbol Table complete

--- symbolsTable.c
+++ symbolsTable.c
@@ -2,4 +2,135 @@
 // Created by nadav on 04/02/2020.
 //
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "symbolsTable.h"
+
+
+
+nodePtr newNode(char *symbolName, int address1, int attachedToGuidance, int isExternal1){
+    nodePtr new;
+    int nameLen = 0;
+    if(symbolName==NULL){
+        return NULL;
+    }
+    nameLen = strlen(symbolName);
+    new = calloc(1, sizeof(node));
+    new->symbolName = calloc(nameLen+1, sizeof(char));
+    strncpy(new->symbolName, symbolName, nameLen);
+    new->counter = 0;
+    new->address = address1;
+    new->isAttachedToGuidance = attachedToGuidance;
+    new->isExternal = isExternal1;
+    return new;
+}
+
+void freeNode(nodePtr toFree){/*a recursive function to free a node ana his next node and so on till last one.*/
+    if(toFree == NULL){
+        return;
+    }
+    if(toFree->symbolName != NULL){
+        free(toFree->symbolName);
+    }
+    freeNode(toFree->next);
+    free(toFree);
+}
+
+linkedListPtr newList()
+{
+    return calloc(1, sizeof(struct linkedList));
+}
+
+void addNodeToEnd(linkedListPtr list, char *symbolName, int address1, int attachedToGuidance, int isExternal1){
+    nodePtr new, nodePtr1;
+    if(list == NULL || symbolName == NULL)
+    {
+        return;
+    }
+    new = newNode(symbolName, address1, attachedToGuidance, isExternal1);
+    if(!list->size){
+        list->head = new;
+    }else{
+        for(nodePtr1 = list->head ; nodePtr1->next; nodePtr1=nodePtr1->next)
+            ;
+    
+        nodePtr1->next = new;
+    }
+    new->next = NULL;
+    new->counter = 1;
+    list->size++;
+
+        
+}
+
+void addNodeToStart(linkedListPtr list, char *symbolName, int address1, int attachedToGuidance, int isExternal1) /*can I discard this function??????????*/
+    {
+        nodePtr new = newNode(symbolName, address1, attachedToGuidance, isExternal1);
+        new->next = list->head;
+        list->head = new;
+        list->size = list->size +1;
+    }
+
+    nodePtr searchSymbolNameInList(char symbolName[], linkedListPtr list)
+    {
+        nodePtr searchedNode = list->head;
+        while (searchedNode!=NULL)
+    {
+        if(strcmp(searchedNode->symbolName,symbolName) == 0)
+            return searchedNode;
+        searchedNode = searchedNode->next;
+    }
+    return searchedNode;
+    }
+
+
+
+    void printList(linkedListPtr listPtr)
+    {
+        int i;
+        nodePtr node = listPtr->head;
+
+        for (i = 0; i < listPtr->size; i++)
+        {
+            printNode(node);
+            printf("                            ||                        \n");
+            printf("                            ||                        \n");
+            node = node->next;
+
+        }
+        
+    }
+
+    void printNode(nodePtr node)
+    {
+        if(!node)
+        {
+            return;
+        }
+        /*if ((node->next == NULL) && node->counter == 1)
+            printf("%s - 1 time\n", node->symbolName);
+
+        else if (node->next == NULL)
+            printf("%s - %d times\n", node->symbolName,node->counter); */
+
+        else
+            printf("[NAME - %s ***  ADDRESS - %d ***  ", node->symbolName, node->address);   
+            if(node->isAttachedToGuidance == 1) printf("is Attached To Guidance? - YES. *** ");
+            else if (node->isAttachedToGuidance == 0) printf("is Attached To Guidance? - NO. *** ");
+
+            if (node->isExternal == 1) printf("is EXTERNAL? - YES. *** ]");
+
+            else if (node->isExternal == 0) printf("is EXTERNAL? - NO. *** \n");
+    }
+
+    int listIsEmpty(linkedListPtr listPtr)
+    {
+        if (listPtr->head == NULL)
+        {
+            return 1;
+        }
+        return 0;     
+    }
+
--- symbolsTable.h
+++ symbolsTable.h
@@ -5,4 +5,40 @@
 #ifndef MAABADA_MMN14_SYMBOLSTABLE_H
 #define MAABADA_MMN14_SYMBOLSTABLE_H
 
+typedef struct node{
+    int counter;/* can I delete this field????????????????? */
+    char *symbolName;
+    int address;/*I did the symbol table as Dani Kalfon instructs in his lecture about the project (at time 2:01 in the video you can see the table...)*/
+    int isAttachedToGuidance;
+    int isExternal;
+    struct node *next;
+}*nodePtr, node;
+
+typedef struct linkedList {
+    int size;
+    nodePtr head;
+}*linkedListPtr;
+
+int parsStr(char *str);
+
+nodePtr newNode(char *symbolName, int address1, int attachedToGuidance, int isExternal1);
+
+void freeNode(nodePtr toFree);
+
+linkedListPtr newList();
+
+void addNodeToEnd(linkedListPtr list, char *symbolName, int address1, int attachedToGuidance, int isExternal1);
+
+void addNodeToStart(linkedListPtr list, char *symbolName, int address1, int attachedToGuidance, int isExternal1);
+
+nodePtr searchSymbolNameInList(char symbolName[], linkedListPtr list);
+
+void printNode(nodePtr node);
+
+void printList(linkedListPtr listPtr);
+
+int listIsEmpty(linkedListPtr listPtr);
+
+int strToInt(char c[]);
+
 #endif //MAABADA_MMN14_SYMBOLSTABLE_H
