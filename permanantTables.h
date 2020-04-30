d9b049bbb9fa859464b58e070affd08058e3e4f7
Nadav Meidan <nadav.medan@mail.huji.ac.il>
2020-04-30 09:36:07 +0300

    fixed the opCode, registers, and instructions tables

--- permanantTables.h
+++ permanantTables.h
@@ -18,90 +18,44 @@ typedef enum instructions {STRING, DATA, ENTRY, EXTERN } ;
 /*!
  *
  */
-typedef struct opTable
+struct opTable
 {
     char *codeName;
     unsigned int word;
 }
-matrix[] =
+commandTable[] =
 {
-        {"mov",
-        (unsigned short)2032}
+        {"mov", (unsigned short) 2032},
 
-        {
-            matrix[1].codeName = "cmp";
-            matrix[1].word = (unsigned short) 4088;
-        }
+        {"cmp", (unsigned short) 4088},
 
-        {
-            matrix[2].codeName = "add";
-            matrix[2].word = (unsigned short) 6128;
-        }
+        {"add", (unsigned short) 6128},
 
-        {
-            matrix[3].codeName = "sub";
-            matrix[3].word = (unsigned short) 8176;
-        }
+        {"sub", (unsigned short) 8176},
 
-        {
-            matrix[4].codeName = "lea";
-            matrix[4].word = (unsigned short) 8560;
-        }
+        {"lea", (unsigned short) 8560},
 
-        {
-            matrix[5].codeName = "clr";
-            matrix[5].word = (unsigned short) 10352;
-        }
+        {"clr", (unsigned short) 10352},
 
-        {
-            matrix[6].codeName = "not";
-            matrix[6].word = (unsigned short) 12400;
-        }
+        {"not", (unsigned short) 12400},
 
-        {
-            matrix[7].codeName = "inc";
-            matrix[7].word = (unsigned short) 14448;
-        }
+        {"inc", (unsigned short) 14448},
 
-        {
-            matrix[8].codeName = "dec";
-            matrix[8].word = (unsigned short) 16496;
-        }
+        {"dec", (unsigned short) 16496},
 
-        {
-            matrix[9].codeName = "jmp";
-            matrix[9].word = (unsigned short) 18480;
-        }
+        {"jmp", (unsigned short) 18480},
 
-        {
-            matrix[10].codeName = "bne";
-            matrix[10].word = (unsigned short) 20528;
-        }
+        {"bne", (unsigned short) 20528},
 
-        {
-            matrix[11].codeName = "red";
-            matrix[11].word = (unsigned short) 22640;
-        }
+        {"red", (unsigned short) 22640},
 
-        {
-            matrix[12].codeName = "prn";
-            matrix[12].word = (unsigned short) 24696;
-        }
+        {"prn", (unsigned short) 24696},
 
-        {
-            matrix[13].codeName = "jsr";
-            matrix[13].word = (unsigned short) 26672;
-        }
+        {"jsr", (unsigned short) 26672},
 
-        {
-            matrix[14].codeName = "rts";
-            matrix[14].word = (unsigned short) 28672;
-        }
+        {"rts", (unsigned short) 28672},
 
-        {
-            matrix[15].codeName = "stop";
-            matrix[15].word = 30720;
-        }
+        {"stop", 30720}
 };
 
 
@@ -115,7 +69,7 @@ char validCommands[16][4] = {"mov", "cmp", "add", "sub", "lea", "clr", "not", "i
 /*!
  *
  */
-typedef struct instruction
+struct instruction
 {
     char* instruction;
     int insType;
@@ -132,7 +86,7 @@ typedef struct instruction
 /*!
  *
  */
-typedef struct Register
+struct Register
 {
     char* regName;
     int regNum;
